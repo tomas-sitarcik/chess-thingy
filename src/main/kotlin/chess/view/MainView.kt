5adfddeebc3f1b8b8343cb71e1f83dbce4eccc5c
yeet <t.sith96@gmail.com>
2022-01-25 20:39:15 +0100

    fxml

--- src/main/kotlin/chess/view/MainView.kt
+++ src/main/kotlin/chess/view/MainView.kt
@@ -7,17 +7,18 @@ import javafx.scene.layout.BorderPane
 import tornadofx.*
 import tornadofx.Controller
 import java.io.File
+import java.lang.Thread.sleep
 
 class MainView : View() {
     override val root : AnchorPane by fxml("/MainView.fxml")
     private val boardImageView : ImageView by fxid("boardImageView")
 
     init {
-        currentStage?.minWidth = 800.0
-        currentStage?.minHeight = 600.0
-        currentStage?.width = 800.0
-        currentStage?.width = 600.0
-        //boardImageView.image = Image("https://pbs.twimg.com/media/FJ4w3tfXwAgHoPi?format=png&name=small")
+        currentStage?.minWidth = 900.0
+        currentStage?.minHeight = 650.0
+        currentStage?.width = 900.0
+        currentStage?.width = 650.0
+        boardImageView.image = Image("https://pbs.twimg.com/media/FJ4w3tfXwAgHoPi?format=png&name=small")
     }
 
     fun print() {
