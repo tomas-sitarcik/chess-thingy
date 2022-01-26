378f5c92578fdfabacb39ed4ef4dec62cc467105
tsiti <t.sith96@gmail.com>
2022-01-26 12:30:38 +0100

    fxml fiddling

--- src/main/kotlin/chess/view/MainView.kt
+++ src/main/kotlin/chess/view/MainView.kt
@@ -1,9 +1,11 @@
 package chess.view
 
+import javafx.scene.canvas.Canvas
 import javafx.scene.image.Image
 import javafx.scene.image.ImageView
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.BorderPane
+import javafx.scene.paint.Color
 import tornadofx.*
 import tornadofx.Controller
 import java.io.File
@@ -11,14 +13,16 @@ import java.lang.Thread.sleep
 
 class MainView : View() {
     override val root : AnchorPane by fxml("/MainView.fxml")
-    private val boardImageView : ImageView by fxid("boardImageView")
+    private val board : Canvas by fxid("board")
 
     init {
         currentStage?.minWidth = 900.0
         currentStage?.minHeight = 650.0
         currentStage?.width = 900.0
         currentStage?.width = 650.0
-        boardImageView.image = Image("https://pbs.twimg.com/media/FJ4w3tfXwAgHoPi?format=png&name=small")
+        val boardGContext = board.graphicsContext2D
+        boardGContext.fill = Color.BLUEVIOLET
+        boardGContext.fillRect(0.0, 0.0, 250.0, 250.0)
     }
 
     fun print() {
