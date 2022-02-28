d1d8c1c565f838af17203c73d6c155b4df6610f4
yeet <t.sith96@gmail.com>
2022-02-28 23:28:25 +0100

    bad omen might have to do some high class magic to get the game class to work, or abandon it and have it all work inside of mainView

--- src/main/kotlin/chess/game/Game.kt
+++ src/main/kotlin/chess/game/Game.kt
@@ -1,10 +1,19 @@
 package chess.game
 
 import chess.board.Piece
+import chess.board.initBoard
+import chess.view.MainView
+import tornadofx.FX.Companion.find
+import tornadofx.UIComponent
+import tornadofx.WorkspaceApp
+import kotlin.reflect.KClass
 
-class Game(var board: Array<Array<Piece?>>, var multiplayer: Boolean = true) { // leaving multiplayer on by default, the AI is the last thing im gonna do
+class Game(initiallyDockedView: KClass<out UIComponent>, val board: Array<Array<Piece?>>, var multiplayer: Boolean = true) : WorkspaceApp(initiallyDockedView) { // leaving multiplayer on by default, the AI is the last thing im gonna do
+
+
 
     fun start() {
-        
+        board = initBoard()
+        var mainView: MainView by find<>()
     }
 }
